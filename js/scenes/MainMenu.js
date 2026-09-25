e1dccd26ff043951e9cf035f9dc4ebb2871b5780
Sid <sid.verma2407@gmail.com>
2026-09-25 22:25:38 +0530

    Update title screen background and add haunted flicker

--- js/scenes/MainMenu.js
+++ js/scenes/MainMenu.js
@@ -3,6 +3,15 @@ class MainMenu {
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         this.camera.position.z = 5;
+        
+        // Background Image Plane
+        const geometry = new THREE.PlaneGeometry(16, 9); // Assuming 16:9 aspect ratio roughly
+        const textureLoader = new THREE.TextureLoader();
+        const texture = textureLoader.load('assets/images/menu-background-new.jpg');
+        const material = new THREE.MeshBasicMaterial({ map: texture, color: 0x555555 }); // Darkened a bit
+        const plane = new THREE.Mesh(geometry, material);
+        plane.position.z = 0;
+        this.scene.add(plane);
     }
     init() {
         console.log("Main Menu Init");
@@ -10,7 +19,9 @@ class MainMenu {
     }
     update(delta) {
         // Slow zoom effect
-        this.camera.position.z -= delta * 0.1;
+        if (this.camera.position.z > 3) {
+            this.camera.position.z -= delta * 0.05;
+        }
     }
     dispose() {
         mainMenuUI.hide();
